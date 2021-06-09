9def9f37b1de99a3a0cdf004f482c630ae959370
gdezayas <gdzayas@gmail.com>
2021-06-09 18:45:24 -0400

    control de ExceptionHandlerController

---
+++ src/main/java/cu/edu/cujae/backend/api/config/exception/ExceptionHandlerController.java
@@ -0,0 +1,14 @@
+package cu.edu.cujae.backend.api.config.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeException(RuntimeException e) {
+        return new ResponseEntity<>("[Exception Response] - Exception: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
