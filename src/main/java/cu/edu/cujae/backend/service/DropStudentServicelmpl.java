a1dbe3c619391f234664820f7d53b87e86318e67
gdezayas <gdzayas@gmail.com>
2021-06-29 16:27:48 -0400

    Baja Estudiante

--- src/main/java/cu/edu/cujae/backend/service/DropStudentServicelmpl.java
+++ src/main/java/cu/edu/cujae/backend/service/DropStudentServicelmpl.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 @Service
@@ -33,21 +30,24 @@ public class DropStudentServicelmpl implements DropStudentService {
     @Override
     public List<DropStudentDto>getDropStudent() throws SQLException {
         List<DropStudentDto> dropStudentList = new ArrayList<DropStudentDto>();
-        ResultSet rs = jdbcTemplate.getDataSource().getConnection().createStatement().executeQuery(
-                "SELECT * from baja"
-        );
-        while (rs.next()){
-            dropStudentList.add(new DropStudentDto(new ReasonDropDto(rs.getInt("cod_motivo"),null),new StudentDto(rs.getInt("cod_estudiante"),null,null,null,null)
-            ));
+        try (Connection con = jdbcTemplate.getDataSource().getConnection()){
+            ResultSet rs = con.createStatement().executeQuery("SELECT * from baja");
+            while (rs.next()){
+                dropStudentList.add(new DropStudentDto(new ReasonDropDto(rs.getInt("cod_motivo"),null),new StudentDto(rs.getInt("cod_estudiante"),null,null,null,null)
+                ));
+            }
+            return dropStudentList;
         }
-        return dropStudentList;
+
     }
 
     @Override
     public void deleteDropStudent(int cod_Student) throws SQLException {
         try (Connection conn = jdbcTemplate.getDataSource().getConnection()){
-            CallableStatement cs = conn.prepareCall("delete_baja(?)");
-            cs.setInt(1,cod_Student);
+            PreparedStatement ps = conn.prepareStatement("delete from baja where cod_estudiante = ?");
+            ps.setInt(1,cod_Student);
+
+            ps.executeUpdate();
         }
     }
 
